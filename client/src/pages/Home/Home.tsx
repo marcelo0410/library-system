522bdec9749c4bc7fbd9e9ebb0d5b7040b3c709f
marcelo0410 <cs95401156@gmail.com>
2023-01-28 02:35:28 +1100

    front end: search function

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { fetchBooks } from '../../api/index';
-import { Table, Space } from 'antd';
+import { Table, Space, Input } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
 import type { ColumnsType, TableProps } from 'antd/es/table';
+import { fetchBooks } from '../../api/index';
 import { Book } from '../../common/interface';
 
 
@@ -53,26 +55,56 @@ const getFullDate = (date: string): string => {
     return dateAndTime[0].split('-').reverse().join('-');
 };
 
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
+);
+
+
+
 
 const onChange: TableProps<Book>['onChange'] = (pagination, filters, sorter, extra) => {
   console.log('params', pagination, filters, sorter, extra);
 };
 
-const App: React.FC = () => {
+const Home: React.FC = () => {
 
     const [bookData, setBookData] = useState<Book[]>([]);
+    const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
+    const [value, setValue] = useState<string>('');
+
+    const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const filteredData = bookData.filter(entry =>
+            entry.title.toLowerCase().includes(event.target.value.toLowerCase())
+        );
+        setFilteredBooks(filteredData);
+    }
 
     useEffect(() => {
-        fetchBooks().then(data => {setBookData(data.data); console.log(data)});
+        fetchBooks().then(data => {setBookData(data.data); setFilteredBooks(data.data);});
     }, [])
 
     return (
         <div>
-            <Table columns={columns} dataSource={bookData} onChange={onChange} />
+            <Space direction='vertical'>
+                <Input
+                    placeholder="input search text"
+                    size="large"
+                    suffix={suffix}
+                    onChange={(event) => onChangeInput(event)}
+                />
+            </Space>
+            <Table columns={columns} dataSource={filteredBooks} onChange={onChange} />
         </div>
         
     )
 }
 
 
-export default App;
+export default Home;
